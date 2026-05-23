ca5b03d8dc204d21ad5bd8b4084292d63194eeed
giursl <giulia.rafalopes@gmail.com>
2026-05-22 23:32:36 -0300

    ocultar_senha

--- static/js/ocultarSenha.js
+++ static/js/ocultarSenha.js
@@ -1,17 +1,17 @@
-const checkbox = document.getElementById("mostrarSenha");
+document.addEventListener("DOMContentLoaded", () => {
+    const checkbox = document.getElementById("mostrarSenha");
+    // Ele vai buscar todos os elementos que têm a classe 'senha'
+    const camposSenha = document.querySelectorAll(".senha");
 
-const camposSenha = document.querySelectorAll(".senha");
-
-checkbox.addEventListener("change", () => {
-
-    camposSenha.forEach((campo) => {
-
-        if (checkbox.checked) {
-            campo.type = "text";
-        } else {
-            campo.type = "password";
-        }
-
-    });
-
+    if (checkbox) {
+        checkbox.addEventListener("change", () => {
+            camposSenha.forEach((campo) => {
+                if (checkbox.checked) {
+                    campo.type = "text";
+                } else {
+                    campo.type = "password";
+                }
+            });
+        });
+    }
 });
