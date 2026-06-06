423862b51a497c3b70f67bee5eab09de50de80ce
giursl <giulia.rafalopes@gmail.com>
2026-06-06 00:22:09 -0300

    settings_email_brevo

--- fe_telecom/settings.py
+++ fe_telecom/settings.py
@@ -157,10 +157,12 @@ EMAIL_HOST_PASSWORD = 'rzzgitbeldtwssbs'
 import os
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')  # Permite mudar pelo painel
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+# Se mudar para o Brevo/Sendgrid usando a porta 2525, mantenha TLS como True
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False  # Garanta que está explicitamente False
+EMAIL_USE_SSL = False
+EMAIL_TIMEOUT = 10
 
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'fetelecomservices@gmail.com')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
