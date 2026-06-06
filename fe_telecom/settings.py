ed64a1b7923d089390fb6926af0496b79676c20b
giursl <giulia.rafalopes@gmail.com>
2026-06-06 00:05:48 -0300

    TESTE_EMAIL

--- fe_telecom/settings.py
+++ fe_telecom/settings.py
@@ -153,3 +153,15 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'fetelecomservices@gmail.com'
 
 EMAIL_HOST_PASSWORD = 'rzzgitbeldtwssbs'
+
+import os
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False  # Garanta que está explicitamente False
+
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'fetelecomservices@gmail.com')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
