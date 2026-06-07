467af654fdc068f0aa53e8b7bbc1f0166ff71fb6
giursl <giulia.rafalopes@gmail.com>
2026-06-07 00:21:31 -0300

    storage _Settings

--- fe_telecom/settings.py
+++ fe_telecom/settings.py
@@ -126,11 +126,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 # CONFIGURAÇÃO DE E-MAIL (VIA API DO BREVO COM ANYMAIL)
