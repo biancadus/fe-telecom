ae24b6ce749b52c83e8c466f40d81f291dd2eef9
giursl <giulia.rafalopes@gmail.com>
2026-06-07 00:15:38 -0300

    settings

--- fe_telecom/settings.py
+++ fe_telecom/settings.py
@@ -121,11 +121,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STORAGES = {
     "staticfiles": {
