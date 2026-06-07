52f4f67b507399db96147200b58d2c0dd16c93ec
giursl <giulia.rafalopes@gmail.com>
2026-06-07 00:24:35 -0300

    another_settingspy

--- fe_telecom/settings.py
+++ fe_telecom/settings.py
@@ -124,7 +124,9 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'core', 'static'),
+]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
